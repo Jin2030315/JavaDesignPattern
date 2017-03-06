ce7d3372d6a9acdc5976271f5dba68a958edb833
jin80 <jin80@192.168.56.1>
2017-03-06 17:04:11 +0800

    Add comments at class of Invoke

--- src/command/pattern/demo/Invoke.java
+++ src/command/pattern/demo/Invoke.java
@@ -3,7 +3,12 @@ package command.pattern.demo;
 public class Invoke {
 	ICommand iCommand;
 	
+	/**
+	 * 
+	 * @param iCommand
+	 */
 	public void ExecuteCommand(ICommand iCommand){
+		
 		this.iCommand = iCommand;
 		iCommand.Do();
 	}
